0f04c9f20e3ea17b4c296b449593394441a43b9d
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-13 15:26:25 +0300

    edit docs speller.py

--- speller.py
+++ speller.py
@@ -3,16 +3,16 @@ import json
 import string
 
 
-def ya_speller(query: str) -> bool:
-    '''Проверяет слова, которые не прошли проверку словарями, с помощью api.'''
+def ya_speller(word: str) -> bool:
+    '''Принимает строку. Возвращает булево значение в зависимости от корректности написания слова.'''
     endpoint = 'https://speller.yandex.net/services/spellservice.json/checkText?'
-    params = {"text": query}
+    params = {"text": word}
 
     with requests.session() as session:
         session.headers['User-Agent'] = 'insomnia/2022.2.1'
 
         response = session.get(endpoint, params=params)
         j_data = json.loads(response.text)
-        if len(j_data) == 0 and query not in string.ascii_letters and query not in string.punctuation:
+        if len(j_data) == 0 and word not in string.ascii_letters and word not in string.punctuation:
             return True
     return False
