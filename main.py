6d99e0568e7bdb0ca5ce1c0439a7e542c1d6ed9b
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-13 15:09:24 +0300

    rewrite main.py

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
 import enchant
-import pandas as pd
-import csv
 from loguru import logger
 import speller
 
-file_name = input('file name: ')  # bucket-25-dress-search
-df = pd.read_csv(f'{file_name}.csv', delimiter='|')
-df['error'] = 0
-false_words_set = []  # список слов до проверки спеллера
-dictionary_ru = enchant.Dict('ru_RU')
-dictionary_custom = enchant.Dict('ru_CUSTOM')
-# C:\CodePy\wb\spellcheck\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
-dictionary_brands = enchant.Dict('brands')  # словарь с брендами
+
+def write(file: str, new_data: list) -> None:
+    '''Примет название файла. Запишет файл с именем result_{file}.csv в папку со сценарием.'''
+    file = file.split('\\')[-1]
+    with open(f'result_{file}.csv', 'w', encoding='utf-8') as file_output:
+        file_output.write(head)
+        file_output.writelines(new_data)
+        logger.info(f'Записано {len(new_data)} строк.')
 
 
-def word_check(item):
-    # проверяет наличие слова в одном из словарей
-    if dictionary_ru.check(item) or dictionary_custom.check(item) or dictionary_brands.check(item):
+def check_word(word: str) -> bool:
+    '''Примет слово. Если слово прошло проверку на грамматику, вернет правду.'''
+    if dictionary_ru.check(word) or dictionary_custom.check(word) or dictionary_brands.check(word):
         return True
-    if item not in false_words_set:
-        false_words_set.append(item)
-        logger.info(f'слово добавлено в false_words_set: {item}')
-        speller.ya_speller(item)
+    else:
+        if word not in false_words_set:
+            false_words_set.append(word)
+            logger.info(f'слово добавлено в false_words_set: {word}')
+            speller.ya_speller(word)
+
     return False
 
 
-def query_check(serie: pd.Series):
-    debracket = serie.replace('(', '').replace(')', '')
-    debracket: str
-    list_from_str = debracket.split()
-    result = all(map(word_check, list_from_str))
-    if not result:
-        logger.info(f'запрос, не прошедший проверку: {list_from_str}')
-    return result
+def check_lines(data: list) -> list:
+    '''Примет список строк из файла. Вернет отредактированный список строк.'''
+    new_data = []
+    for line in data:
+        for word in line.split('|')[0].replace('(', '').replace(')', '').split():
+            if not check_word(word):
+                new_data.append(line)
+    return new_data
 
 
-df['error'] = df['Search Query'].apply(query_check)
+def read(file: str) -> list:
+    '''Примет ссылку на файл без расширения. Вернет список строк файла без первой строки.'''
+    with open(f'{file}.csv', 'r', encoding='utf-8') as file_input:
+        global head
+        head = file_input.readline()
+        data = file_input.readlines()
+        logger.info(f'Прочитано {len(data)} строк.')
+    return data
 
-trunc_df = df.loc[df['error'] == False]
-trunc_df.to_csv(f'result_{file_name}.csv', sep='|', quoting=csv.QUOTE_NONE)
-print(*false_words_set, sep='\n',
-      file=open('reports\\result_words_for_speller.txt', 'w', encoding='utf-8'))
 
-print(*speller.true_words, sep='\n',
-      file=open('reports\\result_true.txt', 'w', encoding='utf-8'))  # слова, прошедшие проверку спеллера
-print(*speller.false_words, sep='\n',
-      file=open('reports\\result_false.txt', 'w', encoding='utf-8'))  # слова, не прошедшие проверку спеллера
+def main() -> None:
+    data = read(file := input('file name: '))  # bucket-25-dress-search
+    new_data = check_lines(data)
+    write(file, new_data)
 
+
+if __name__ == '__main__':
+    false_words_set = []  # список слов до проверки спеллера
+    dictionary_ru = enchant.Dict('ru_RU')  # русский словарь из Либры
+    dictionary_custom = enchant.Dict('ru_CUSTOM')  # пользовательский словарь
+    dictionary_brands = enchant.Dict('brands')  # словарь с брендами
+    # C:\CodePy\wb\spellcheck\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
+    main()
