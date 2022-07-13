33dafcf227971f4073f72fd70699a6bfbea53db8
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-13 15:16:38 +0300

    rewrite speller.py

--- speller.py
+++ speller.py
@@ -3,12 +3,8 @@ import json
 import string
 
 
-true_words = []  # список из слов, прошедших проверку
-false_words = []  # список слов, не прошедших проверку
-
-
-def ya_speller(query: str):
-    # проверяет слова, которые не прошли проверку словарями, с помощью api
+def ya_speller(query: str) -> bool:
+    '''Проверяет слова, которые не прошли проверку словарями, с помощью api.'''
     endpoint = 'https://speller.yandex.net/services/spellservice.json/checkText?'
     params = {"text": query}
 
@@ -18,6 +14,5 @@ def ya_speller(query: str):
         response = session.get(endpoint, params=params)
         j_data = json.loads(response.text)
         if len(j_data) == 0 and query not in string.ascii_letters and query not in string.punctuation:
-            true_words.append(query)
-        else:
-            false_words.append(query)
+            return True
+    return False
